0493f71e8e476fc8467ee417cf90644c3ea43452
Sofia Mondello <sofiarmondello@gmail.com>
2023-10-19 21:58:51 -0300

    elimino carpeta src por fuera dle proyecto

--- src/main/java/com/example/demo/controller/CarreraControllerJPA.java
+++ src/main/java/com/example/demo/controller/CarreraControllerJPA.java
@@ -2,11 +2,8 @@ package com.example.demo.controller;
 
 import java.util.List;
 import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,8 +11,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
 
 import com.example.demo.Service.CarreraService;
 import com.example.demo.dto.ReporteCarrerasDTO;
